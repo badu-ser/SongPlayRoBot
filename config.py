e565f23627a7547a19239f5c2a1581e68b026979
Badu_Ser <100788930+badu-ser@users.noreply.github.com>
2024-04-07 18:46:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = 9023243
-    API_HASH = bbf2755ca5580b90e692fc6d08d19a41
+    API_ID = '9023243'
+    API_HASH = 'bbf2755ca5580b90e692fc6d08d19a41'
     BOT_TOKEN = '5138752061:AAGabbJ_N0vOkvmokPjW4uG2ifKvxRrwCiA'
